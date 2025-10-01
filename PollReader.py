0730f3ffac1b9781ac5535f96b63277c91678d4a
Julian Liao <jliao10@Julians-MacBook-Pro-5.local>
2025-10-01 17:31:24 -0400

    Fixed first function and added first function

--- PollReader.py
+++ PollReader.py
@@ -55,31 +55,31 @@ class PollReader():
         """
 
         # iterate through each row of the data
-        for i in self.raw_data:
+        for i in self.raw_data[1:]:
 
             # split up the row by column
-            seperated = i.split(' ')
+            seperated = i.strip().split(',')
 
             # map each part of the row to the correct column
             self.data_dict['month'].append(seperated[0])
             self.data_dict['date'].append(int(seperated[1]))
-            self.data_dict['sample'].append(int(seperated[2]))
-            self.data_dict['sample type'].append(seperated[2])
+            self.data_dict['sample'].append(int(seperated[2].split(" ")[0]))
+            self.data_dict['sample type'].append(seperated[2].split(" ")[1])
             self.data_dict['Harris result'].append(float(seperated[3]))
             self.data_dict['Trump result'].append(float(seperated[4]))
 
 
     def highest_polling_candidate(self):
-        """
-        This method should iterate through the result columns and return
-        the name of the candidate with the highest single polling percentage
-        alongside the highest single polling percentage.
-        If equal, return the highest single polling percentage and "EVEN".
-
-        Returns:
-            str: A string indicating the candidate with the highest polling percentage or EVEN,
-             and the highest polling percentage.
-        """
+    # Find max value in Harris's results
+        max_harris = max(self.data_dict['Harris result'])
+        max_trump = max(self.data_dict['Trump result'])
+        # Compare the two
+        if max_harris > max_trump:
+            return f"Harris with{(max_harris * 100):.1f}%"
+        elif max_trump > max_harris:
+            return f"Trump with{(max_trump * 100):.1f}%"
+        else:  # tie
+            return f"EVEN {max_harris:.1f}%"    
         pass
 
 
